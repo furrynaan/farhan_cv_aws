69885441b6c14783b094f7d6ebb50b30bc15a6ab
Farhan <farhan@restech.co.uk>
2024-04-25 23:43:33 +0100

    update code

--- src/app.py
+++ src/app.py
@@ -9,48 +9,40 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-dynamodb_client = boto3.client('dynamodb')
+dynamodb_resource = boto3.resource('dynamodb', 
+                          region_name='us-east-1' # use your region_name
+                          )
 
 def lambda_handler(event, context):
   table = os.environ.get('DDB_TABLE')
   logging.info(f"## Loaded table name from environemt variable DDB_TABLE: {table}")
-  if event["body"]:
-      item = json.loads(event["body"])
-      logging.info(f"## Received payload: {item}")
-      currentnumber = item["currentnumber"]
-      currentnumber += currentnumber
-      update_item_exiting_attribute(currentnumber)
-      message = "Successfully inserted data!"
-      return {
-          "statusCode": 200,
-          "headers": {
-              "Content-Type": "application/json"
-          },
-          "body": json.dumps({"message": message})
-      }
-  else:
-      logging.info("## Received request without a payload")
-      dynamodb_client.put_item(TableName=table,Item={"year": {'N':'2012'}, "title": {'S':'The Amazing Spider-Man 2'}})
-      message = "Successfully inserted data!"
-      return {
-          "statusCode": 200,
-          "headers": {
-              "Content-Type": "application/json"
-          },
-          "body": json.dumps({"message": message})
-      }
+  dynamodbtable = dynamodb_resource.Table(table)
+  item = dynamodbtable.get_item(TableName=table, Key={'id':1})
+  currentnumber = item["Item"]["visitcount"]
+  currentnumber += 1
+  update_item_exiting_attribute(currentnumber)
+  message = "Successfully inserted data!"
+  return {
+      "statusCode": 200,
+      "headers": {
+          'Access-Control-Allow-Origin': '*'
+          
+      },
+      "body": json.dumps({"count": str(currentnumber)})
+  }
+
       
 def update_item_exiting_attribute(currentnumber):
     table = os.environ.get('DDB_TABLE')
-    response = dynamodb_client.update_item(
-        Key={"id": {"N" : "1"}},
+    dynamodbtable = dynamodb_resource.Table(table)
+    response = dynamodbtable.update_item(
+        Key={"id": 1},
         UpdateExpression="set #visitcount = :n",
         ExpressionAttributeNames={
             "#visitcount": "visitcount",
         },
         ExpressionAttributeValues={
-            ":n": {"N" : currentnumber,
-        }},
-        ReturnValues="UPDATED_NEW",
-        TableName=table
+            ":n": str(currentnumber),
+        },
+        ReturnValues="UPDATED_NEW"
     )
