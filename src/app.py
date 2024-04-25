0beb52ccbcb7316bd42f71be42796911374f33e9
Farhan <farhan@restech.co.uk>
2024-04-26 00:14:57 +0100

    added exception handling

--- src/app.py
+++ src/app.py
@@ -9,6 +9,8 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+dynamodb_client = boto3.client('dynamodb')
+
 dynamodb_resource = boto3.resource('dynamodb', 
                           region_name='us-east-1' # use your region_name
                           )
@@ -16,12 +18,26 @@ dynamodb_resource = boto3.resource('dynamodb',
 def lambda_handler(event, context):
   table = os.environ.get('DDB_TABLE')
   logging.info(f"## Loaded table name from environemt variable DDB_TABLE: {table}")
+
+      
   dynamodbtable = dynamodb_resource.Table(table)
+  
   item = dynamodbtable.get_item(TableName=table, Key={'id':1})
-  currentnumber = item["Item"]["visitcount"]
+  
+  try:
+    currentnumber = int(item["Item"]["visitcount"])
+  except ValueError:
+      return {
+      "statusCode": 200,
+      "headers": {
+          'Access-Control-Allow-Origin': '*'
+          
+      },
+      "body": json.dumps({"message": "error converting value"})
+  }
+    
   currentnumber += 1
   update_item_exiting_attribute(currentnumber)
-  message = "Successfully inserted data!"
   return {
       "statusCode": 200,
       "headers": {
@@ -42,7 +58,7 @@ def update_item_exiting_attribute(currentnumber):
             "#visitcount": "visitcount",
         },
         ExpressionAttributeValues={
-            ":n": str(currentnumber),
+            ":n": currentnumber,
         },
         ReturnValues="UPDATED_NEW"
     )
